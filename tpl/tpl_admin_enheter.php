3315b1b7bf469e015d92685581e59cc90b8f2d56
Bjørn Kristiansen <post@bhkristiansen.com>
2025-06-18 23:53:11 +0200

    various

--- tpl/tpl_admin_enheter.php
+++ tpl/tpl_admin_enheter.php
@@ -11,9 +11,12 @@ echo '
          <ul class="art">';
 
 foreach($tpl_params['machines'] as $id => $b) {
+    $b['jpg'] = isset($b['jpg']) ? $b['jpg'] : '';
+    $is_thumb = file_exists(SITEDIR . '/ext/jpg_thumb/'.$b['jpg']) ? '_thumb' : '';
+
     echo '
-            <li class="machs status' , !empty($b['status']) ? $b['status'] : '' , '">
-                ' , !empty($b['jpg']) ? '<a href="' . SITEURL . '/admin/enheter/edit/' . $id . '"><img class="thumb" src="' . SITEURL . '/ext/jpg/'.$b['jpg'].'" alt="" /></a>' : '' , '
+            <li class="machs status' , !empty($b['status']) ? $b['status'] : '' , empty($is_thumb) ? ' nothumb' : '' , '">
+                ' , !empty($b['jpg']) ? '<a href="' . SITEURL . '/admin/enheter/edit/' . $id . '"><img class="thumb" src="' . SITEURL . '/ext/jpg' . $is_thumb . '/'.$b['jpg'].'" alt="" /></a>' : '' , '
                 <dl>
                     <dt>Navn</dt><dd><a href="' . SITEURL . '/admin/enheter/edit/' . $id . '">' , $b['tittel'] , '</a></dd>
                     <dt>Slette</dt><dd><a onclick="return confirm(\'Er du sikker på du vil slette enhet ' . $b['id'] . ' (' . $b['tittel'] . '?\');" href="' . SITEURL . '/admin/enheter/slette/'. $b['id'] .'">bilde</a></dd>
