40ef6f85cedfdf7031ffe332da86b3c2ad3f0f48
Bjørn Kristiansen <post@bhkristiansen.com>
2025-07-01 08:42:35 +0000

    Jul 1, 2025, 10:42 AM

--- tpl/tpl_header_guest.php
+++ tpl/tpl_header_guest.php
@@ -6,7 +6,7 @@ if (!defined('APG'))
 
 echo '
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" id="gall">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="' , SITEURL , '/ext/site.css?v=2">
